c611af35d7aaf0de1a97c038feb21b3eb7629709
Sean Quillen <sean.r.quillen@gmail.com>
2020-09-15 15:27:09 -0500

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
+
+import App from 'next/app'
+import Router from 'next/router'
+import { initGA, logPageView } from '../utils/analytics'
 import wrapper from '../src/store/store'
 import '../styles/globals.css'
 
-const App = ({ Component, pageProps }) => {
-  return (
-    <Component {...pageProps} />
-  )
+class MyApp extends App {
+  componentDidMount () {
+    initGA()
+    logPageView()
+    Router.events.on('routeChangeComplete', logPageView)
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
 }
 
-export default wrapper.withRedux(App)
+export default wrapper.withRedux(MyApp)

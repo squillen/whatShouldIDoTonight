d65dc18d0cd0cbbfb08ce0667a09d9200d804167
Sean Quillen <sean.r.quillen@gmail.com>
2020-09-29 11:20:30 -0500

    change to css

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const path = require('path')
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  },
   webpack: (config, { isServer, dev }) => {
     if (isServer) {
       require('./scripts/generate-sitemap')
@@ -11,16 +7,6 @@ module.exports = {
     if (dev) {
       config.devtool = 'cheap-module-source-map'
     }
-
-    // config.module.rules.push({
-    //   test: /\.scss$/,
-    //   use: [
-    //     options.defaultLoaders.babel,
-    //     {
-    //       loader: '@mdx-js/loader'
-    //     }
-    //   ]
-    // })
 
     return config
   }

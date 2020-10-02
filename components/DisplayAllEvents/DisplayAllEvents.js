5dc3dd92a2265d6d5e60b38f08bb9d4a462ab635
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-02 09:01:41 -0500

    redo all category page

--- components/DisplayAllEvents/DisplayAllEvents.js
+++ components/DisplayAllEvents/DisplayAllEvents.js
@@ -14,77 +14,24 @@ export default class DisplayAllEvents extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      filters: {},
-      categories: [],
       currentActivities: []
     }
   }
 
   componentDidMount () {
-    this.getActivities()
+    this.setState({ currentActivities: this.props.activities })
   }
 
   componentDidUpdate (prevProps) {
-    const { currentActivities } = this.state
     const prevPropActivities = prevProps.activities || []
     const activities = this.props.activities || []
-    if (prevPropActivities.length === 0 && activities.length > 0 && currentActivities && !currentActivities.length) {
-      this.getActivities()
+    if (prevPropActivities.length !== activities.length) {
+      this.setState({ currentActivities: activities })
     }
-  }
-
-  onClick (c) {
-    return () => {
-      const { filters } = this.state
-      const filtersCopy = Object.assign({}, filters)
-      filtersCopy[c] = !filtersCopy[c]
-      this.setState({ filters: filtersCopy }, this.getNewActivities)
-    }
-  }
-
-  getActivities () {
-    const { activities } = this.props
-    const filters = activities && activities.reduce((obj, el) => {
-      el.categories.forEach(category => {
-        obj[category] = obj[category] || true
-      })
-      return obj
-    }, {})
-    const categories = filters && Object.keys(filters)
-    this.setState({ filters, categories, currentActivities: activities })
-  }
-
-  getNewActivities () {
-    const { filters } = this.state
-    const { activities } = this.props
-    const newActivities = {}
-    for (const key in filters) {
-      // only add if selected
-      if (filters[key]) {
-        activities.forEach(a => {
-          // if user selected checkbox, check categories against selection
-          if (a.categories.includes(key)) {
-            // don't duplicate activity
-            if (!newActivities[a.name]) newActivities[a.name] = a
-          }
-        })
-      }
-    }
-    const currentActivities = Object.values(newActivities)
-    this.setState({ currentActivities })
-  }
-
-  handleCategory (c) {
-    if (c === 'selfImprovement') return 'self improvement'
-    if (c === '') return 'free'
-    if (c === 'calm') return 'chill'
-    if (c === 'food') return 'food & drink'
-    if (c === 'read') return 'read & write'
-    return c
   }
 
   render () {
-    const { categories, currentActivities, filters } = this.state
+    const { currentActivities } = this.state
     const { header = 'Do all the stuffs!', source, back } = this.props
     return (
       <motion.div variants={stagger}>
@@ -96,36 +43,15 @@ export default class DisplayAllEvents extends React.Component {
           <div className={styles.header}>{header}</div>
           <div className={styles.count}>{(currentActivities && Array.isArray(currentActivities) && currentActivities.length) || 0} items</div>
         </div>
-        <div className={styles.contentContainer}>
-          <motion.div variants={stagger} className={styles.activitiesContainer}>
-
-            {
-              currentActivities
-
-                ? currentActivities.map(activity => (
-                  <ContentCard key={activity.name} activity={activity} source={source} />
-                ))
-                : <Loading loading={true} />
-            }
-          </motion.div>
-          <div className={styles.filters}>
-            {
-              categories && categories.map(c => {
-                return (
-                  <div
-                    key={c}
-                    className={styles.filter}
-                  >
-                    <div onClick={this.onClick(c)} className={styles[filters[c] ? 'selectedCheckbox' : 'emptyCheckbox']}>
-                      {filters[c] && <span>&#10003;</span>}
-                    </div>
-                    <div className={styles.label}>{this.handleCategory(c)}</div>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
+        <motion.div variants={stagger} className={styles.activitiesContainer}>
+          {
+            currentActivities
+              ? currentActivities.map(activity => (
+                <ContentCard key={activity.name} activity={activity} source={source} />
+              ))
+              : <Loading loading={true} />
+          }
+        </motion.div>
       </motion.div>
     )
   }

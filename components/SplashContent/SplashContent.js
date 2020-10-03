796287f26a2e81e80e8a1d48c32ff6af0f546347
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-03 18:19:01 -0500

    fix mobile bugs

--- components/SplashContent/SplashContent.js
+++ components/SplashContent/SplashContent.js
@@ -9,7 +9,7 @@ function SplashContent ({ content = [], banner, source }) {
   const getContentURL = currContent => currContent && currContent.name
     ? currContent.pagePath || `/${source}/activity?id=${currContent._id}`
     : ''
-  const height = banner ? '90%' : '100%'
+  const minHeight = banner ? '90%' : '100%'
   const getContentDiv = currContent => (
     <div className={styles.background} style={{ background: getBackground(currContent), backgroundSize: 'cover' }}>
       <div className={styles.overlay} />
@@ -29,7 +29,7 @@ function SplashContent ({ content = [], banner, source }) {
           )
           : null
       }
-      <div className={styles.imagesContainer} style={{ height }}>
+      <div className={styles.imagesContainer} style={{ minHeight }}>
         <div className={styles.largeImageContainer}>
           <div
             className={styles.largeImage}

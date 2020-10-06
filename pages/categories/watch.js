7234eab8afad0d18f413421e3a050b9d24abc2b7
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-06 09:31:29 -0500

    improve content handling

--- pages/categories/watch.js
+++ pages/categories/watch.js
@@ -1,142 +1,82 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
-import { motion } from 'framer-motion'
 import PropTypes from 'prop-types'
+
+// REDUX
+import wrapper from '../../src/store/store'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { setWatchActivities } from '../../src/store/categories/action'
 
 // COMPONENTS
 import Layout from '../../components/layout/layout'
 import SplashContent from '../../components/SplashContent/SplashContent'
+import HandleContent from '../../components/HandleContent'
 import { siteTitle } from '../../components/defaultHead'
-import ContentCallOut from '../../components/ContentCallOut/ContentCallOut'
 
 // HELPERS
 import utilStyles from '../../styles/utils.module.css'
-import callAPI from '../../lib/helpers/callAPI'
-import displayContent from '../../lib/helpers/displayContent'
-import displayCategoryOptions from '../../lib/helpers/displayCategoryOptions'
-import { stagger } from '../../animations/default'
-import { slice, findCallOut } from '../../lib/helpers/dataHelpers'
+import { getActivitiesFromDB } from '../../lib/helpers/db/requests'
 
-export async function getStaticProps () {
-  let spotlight = []
-  let ideas = []
-  let free = []
-  let all = []
-
-  try {
-    const handleCall = (path) => callAPI(`watch?${path}`).catch(console.error)
-    const promises = await Promise.all([
-      handleCall('spotlight=spotlight'),
-      handleCall('free=free'),
-      handleCall('ideas=ideas'),
-      handleCall('all=all')
-    ])
-    spotlight = promises[0] || []
-    free = promises[1] || []
-    ideas = promises[2] || []
-    all = promises[3] || {}
-  } catch (e) {
-    console.error(e)
-  }
-  return {
-    props: {
-      spotlight,
-      ideas,
-      free,
-      all
+export const getStaticProps = wrapper.getStaticProps(({ store }) => {
+  const state = store.getState()
+  const all = state.category.watchActivities.all || {}
+  const spotlight = state.category.watchActivities.spotlight || []
+  if (spotlight && !spotlight.length) return getActivitiesFromDB('watch')
+  else {
+    return {
+      props: {
+        spotlight,
+        all
+      }
     }
   }
-}
+})
 
-function WatchSection ({ spotlight, free, ideas, all = {} }) {
-  const obj = all || {}
-  const { total, absurd, nature, romance, paranormal, international, documentary, movies, comedy, horror, drama, action } = obj
+function WatchSection ({ spotlight = [], all = {}, setInRedux, setWatchActivitiesFromProps }) {
+  const [updatedRedux, setUpdatedRedux] = useState(false)
+  if (!updatedRedux && setInRedux) {
+    setUpdatedRedux(true)
+    setWatchActivitiesFromProps({ all, spotlight })
+  }
   const source = 'watch'
-  const contentCategories = [
-    { content: slice(total), header: 'All', source, ref: useRef('All') },
-    { content: slice(absurd), header: 'Absurdist Comedy', source, ref: useRef('Absurdist Comedy') },
-    { content: slice(action), header: 'Action', source, ref: useRef('Action') },
-    { content: slice(comedy), header: 'Comedy', source, ref: useRef('Comedy') },
-    { content: slice(documentary), header: 'Documentaries', source, ref: useRef('Documentaries') },
-    { content: slice(drama), header: 'Drama', source, ref: useRef('Drama') },
-    { content: slice(free), header: 'Free', source, ref: useRef('Free') },
-    { content: slice(horror), header: 'Horror', source, ref: useRef('Horror') },
-    { content: slice(ideas), header: 'Ideas', source, ref: useRef('Ideas') },
-    { content: slice(international), header: 'International', source, ref: useRef('International') },
-    { content: slice(movies), header: 'Movies', source, ref: useRef('Movies') },
-    { content: slice(nature), header: 'Nature', source, ref: useRef('Nature') },
-    { content: slice(paranormal), header: 'Paranormal', source, ref: useRef('Paranormal') },
-    { content: slice(romance), header: 'Romance', source, ref: useRef('Romance') }
+  const obj = all || {}
+  const quotes = [
+    {
+      header: '"Everyone has a purpose in life. Perhaps yours is watching television."',
+      contents: ['-David Letterman', 'Absolutely inspiring.']
+    },
+    {
+      header: '"I really like watching TV at home."',
+      contents: ['-Fred Armisen']
+    },
+    {
+      header: '"I live for watching TV and partying with my book club."',
+      contents: ['-Lauren Lapkus', 'Truth.']
+    }
   ]
 
-  const dramaCallOut = findCallOut(drama)
-  const freeCallOut = findCallOut(drama, dramaCallOut)
-  const horrorCallOut = findCallOut(horror)
-  const categoryOptions = contentCategories.map(displayCategoryOptions)
-  const displayedContent1 = contentCategories.slice(0, 2).map(displayContent)
-  const displayedContent2 = contentCategories.slice(2, 3).map(displayContent)
-  const displayedContent3 = contentCategories.slice(3, 5).map(displayContent)
-  const displayedContent4 = contentCategories.slice(5, 7).map(displayContent)
-  const displayedContent5 = contentCategories.slice(7, 9).map(displayContent)
-  const displayedContent6 = contentCategories.slice(9, contentCategories.length).map(displayContent)
-  const davidLetterman = {
-    header: '"Everyone has a purpose in life. Perhaps yours is watching television."',
-    contents: ['-David Letterman', 'Absolutely inspiring.']
-  }
-  const fredArmisen = {
-    header: '"I really like watching TV at home."',
-    contents: ['-Fred Armisen']
-  }
-  const laurenLapkus = {
-    header: '"I live for watching TV and partying with my book club."',
-    contents: ['-Lauren Lapkus', 'Truth.']
-  }
+  const display = (
+    <HandleContent
+      all={obj}
+      source={source}
+      quotes={quotes}
+    />
+  )
+
   return (
     <Layout>
       <Head>
-        <title>What to watch - {siteTitle}</title>
+        <title>What to Watch - {siteTitle}</title>
       </Head>
-      <div className={utilStyles.tvContainer}>
-        {
-          spotlight && Array.isArray(spotlight) && spotlight.length && <SplashContent content={spotlight} banner="Watch the less known" source={source} />
-        }
-        <div className={utilStyles.infoContainer}>
-          <div className={utilStyles.infoHeader}>We&apos;ve watched thousands of hours of less-known TV for you.</div>
-          <div className={utilStyles.infoBody}>
-            <p>Totally for research.</p>
-          </div>
+      {spotlight && Array.isArray(spotlight) && spotlight.length && <SplashContent content={spotlight} banner="Watch the less known" source={source} />}
+      <div className={utilStyles.infoContainer}>
+        <div className={utilStyles.infoHeader}>We&apos;ve watched thousands of hours of less-known TV for you.</div>
+        <div className={utilStyles.infoBody}>
+          <p>Totally for research.</p>
         </div>
-        <motion.div variants={stagger} className={utilStyles.categoryOptions}>
-          {categoryOptions}
-        </motion.div>
-        {displayedContent1}
-        <ContentCallOut source="watch" item={dramaCallOut} />
-        {displayedContent2}
-        <ContentCallOut source="watch" item={freeCallOut} />
-        {displayedContent3}
-        <ContentCallOut item={fredArmisen} />
-        {displayedContent4}
-        {
-          displayedContent5.length
-            ? (
-              <>
-                <ContentCallOut item={laurenLapkus} />
-                {displayedContent5}
-              </>
-            )
-            : null
-        }
-        {
-          displayedContent6.length
-            ? (
-              <>
-                <ContentCallOut source={source} item={horrorCallOut} />
-                {displayedContent6}
-              </>
-            )
-            : null
-        }
       </div>
+      {display}
     </Layout>
   )
 }
@@ -144,8 +84,14 @@ function WatchSection ({ spotlight, free, ideas, all = {} }) {
 WatchSection.propTypes = {
   spotlight: PropTypes.array,
   all: PropTypes.object,
-  free: PropTypes.array,
-  ideas: PropTypes.array
+  setWatchActivitiesFromProps: PropTypes.func,
+  setInRedux: PropTypes.string
 }
 
-export default WatchSection
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setWatchActivitiesFromProps: bindActionCreators(setWatchActivities, dispatch)
+  }
+}
+
+export default connect((state) => state, mapDispatchToProps)(WatchSection)

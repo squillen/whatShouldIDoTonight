9a25a49bfd7357112b7f7d768704ab76aa3ae983
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-06 09:09:16 -0500

    use cached client

--- middleware/database.js
+++ middleware/database.js
@@ -4,16 +4,28 @@ import nextConnect from 'next-connect'
 const uri = process.env.MONGODB_URI
 const dbName = process.env.MONGODB_DB
 
+let cachedClient = null
+let cachedDb = null
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
 async function database (req, res, next) {
+  if (cachedClient && cachedDb) {
+    req.dbClient = cachedClient
+    req.db = cachedDb
+    req.closeDB = cachedClient.close
+    return next()
+  }
   try {
     if (!client.isConnected()) await client.connect()
+    const db = client.db(dbName)
+    cachedClient = client
+    cachedDb = db
     req.dbClient = client
-    req.db = client.db(dbName)
+    req.db = db
     req.closeDB = client.close
   } catch (e) {
     console.error(e)

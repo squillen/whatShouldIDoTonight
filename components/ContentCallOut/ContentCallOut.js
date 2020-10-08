fc0b523b5fd3108e59d8baf15c3fe3150f01cb96
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-08 14:14:36 -0500

    asdd as ref to link

--- components/ContentCallOut/ContentCallOut.js
+++ components/ContentCallOut/ContentCallOut.js
@@ -5,12 +5,12 @@ export default function ContentCallOut ({ source, item = {}, color = 'darkBlue' 
   const getStyle = (image) => {
     const backgroundColor = {
       blue: 'rgba(25,110,171,0.999)',
-      darkBlue: '#0d2035'
+      darkBlue: '#0d2035',
     }[color]
     return {
       background: image
         ? `url(${image}) center no-repeat`
-        : backgroundColor
+        : backgroundColor,
     }
   }
   const createContent = () => {
@@ -51,10 +51,11 @@ export default function ContentCallOut ({ source, item = {}, color = 'darkBlue' 
         ? handleBody()
         : null
   )
+  const href = item.pagePath || `/${source}/activity?id=${item._id}` || ''
   return (
     item.pagePath || source
       ? (
-        <Link href={item.pagePath || `/${source}/activity?id=${item._id}` || ''}>
+        <Link href={href} as={href}>
           <a className={styles.contentLink}>
             {contents}
           </a>
@@ -68,5 +69,5 @@ ContentCallOut.propTypes = {
   item: PropTypes.object,
   source: PropTypes.string,
   body: PropTypes.object,
-  color: PropTypes.string
+  color: PropTypes.string,
 }

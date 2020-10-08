97d3f57abb4088a39f431ef035930626e4e21f13
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-07 19:47:12 -0500

    url fix attempt

--- components/ContentCard/ContentCard.js
+++ components/ContentCard/ContentCard.js
@@ -6,7 +6,7 @@ import { fadeInFromLeft } from '../../animations/default'
 import styles from './ContentCard.module.css'
 
 export default function ContentCard ({ activity, source }) {
-  const currentURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.SITE_URI
+  const currentURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.SITE_URI || 'https://whatshouldidotonight.com'
   const getBackground = el => `url(${el.image}) center no-repeat`
   const getContentURL = el => el.pagePath ? `${currentURL}${el.pagePath}` : `/${source}/activity?id=${el._id}`
   return (

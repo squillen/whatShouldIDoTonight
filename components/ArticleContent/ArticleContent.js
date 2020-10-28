70790cec219394af7d14e6c74e07b5020d1191c9
Sean Quillen <sean.r.quillen@gmail.com>
2020-10-28 13:53:30 -0500

    add defensive coding

--- components/ArticleContent/ArticleContent.js
+++ components/ArticleContent/ArticleContent.js
@@ -4,33 +4,40 @@ import styles from './ArticleContent.module.css'
 
 export default function ArticleContent ({ articles, banner = 'THE LATEST', source }) {
   const getContentURL = currContent => currContent && currContent.name
-    ? currContent.pagePath || `/${source}/article?id=${currContent._id}`
+    ? currContent.pagePath || `/${source}/${source === 'watch' ? 'article' : 'activity'}?id=${currContent._id}`
     : ''
+  const start = articles.length > 3 ? articles.length - 4 : articles.length > 2 ? articles.length - 3 : null
+  let displayedArticles = start ? articles.slice(start, articles.length) : []
+  displayedArticles = displayedArticles.reverse()
   return (
-    <div className={styles.articleContentContainer}>
-      <div className={styles.articleContentBanner}>{banner}</div>
-      <div className={styles.articlesContainer}>
-        {
-          articles.map(article => {
-            const style = { background: `url(${article.image}) center no-repeat`, backgroundSize: 'cover' }
-            return (
-              <div key={article.name || article.tagline} style={style} className={styles.articleContainer}>
-                <div className={styles.overlay} />
-                <div className={styles.articleTitleContainer}>
-                  <div className={styles.articleTitleText}>
-                    <Link href={getContentURL(article)}>
-                      <a>
-                        {article.name || article.tagline}
-                      </a>
-                    </Link>
+    displayedArticles.length
+      ? (
+        <div className={styles.articleContentContainer}>
+          <div className={styles.articleContentBanner}>{banner}</div>
+          <div className={styles.articlesContainer}>
+            {
+              displayedArticles.map(article => {
+                const style = { background: `url(${article.image}) center no-repeat`, backgroundSize: 'cover' }
+                return (
+                  <div key={article.name || article.tagline} style={style} className={styles.articleContainer}>
+                    <div className={styles.overlay} />
+                    <div className={styles.articleTitleContainer}>
+                      <div className={styles.articleTitleText}>
+                        <Link href={getContentURL(article)}>
+                          <a>
+                            {article.name || article.tagline}
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      )
+      : null
   )
 }
 

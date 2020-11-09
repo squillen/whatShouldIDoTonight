77fdfdecc48a2b58b25ddc7180e730fc4e6eca77
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-09 15:42:03 -0600

    add in potential for table of contents

--- components/ArticleDisplay/ArticleDisplay.js
+++ components/ArticleDisplay/ArticleDisplay.js
@@ -1,3 +1,4 @@
+import React, { useRef } from 'react'
 import styles from './ArticleDisplay.module.css'
 import PropTypes from 'prop-types'
 import IFrame from '../IFrame/IFrame'
@@ -10,6 +11,21 @@ import handleMarkdown from '../../lib/helpers/handleMarkdown'
 
 export default function ArticleDisplay ({ article, source }) {
   const style = { background: `url(${article.image}) center no-repeat`, backgroundSize: 'cover' }
+  let refs
+  const articleHeaders = []
+  const toc = article.body[0].toc || []
+  if (toc) {
+    refs = {}
+    article.body.map(obj => {
+      const name = obj.name || ''
+      articleHeaders.push(name)
+      refs[name] = useRef(name)
+    })
+  }
+  const handleRef = name => {
+    return refs ? refs[name] : null
+  }
+
   return (
     article &&
     <>
@@ -22,17 +38,22 @@ export default function ArticleDisplay ({ article, source }) {
           <h1 className={styles.hidden}>i we are bored</h1>
         </div>
         <BackButton />
+        {/* {
+          articleHeaders.length && refs && articleHeaders.map(item => (
+            <div key={item} className="tocItem" onClick={() => refs[item].current.scrollIntoView({ behavior: 'smooth' })}>{item.split('## ')[1]}</div>
+          ))
+        } */}
         <div className={styles.articleBody}>
           {
             article.body.map((item, idx) => (
-              <div key={item.name || idx} className={styles.bodyContainer}>
+              <div key={item.name || idx} className={styles.bodyContainer} ref={() => handleRef(item.name)}>
                 {
                   item.name && (
                     <div className={styles.suggestionHeader}>
                       <div className={styles.blob}>
                         {<SVGGrabber type="circle" />}
                       </div>
-                      {handleMarkdown(item.name[0] === '#' ? item.name : `# ${item.name}`)}
+                      {handleMarkdown(item.name[0] === '#' ? item.name : `## ${item.name}`)}
                     </div>
                   )
                 }

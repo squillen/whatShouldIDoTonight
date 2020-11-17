73f6fce7f0edd5e3f155418403588b4694e2f87a
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-17 17:27:47 -0600

    new layout improvements

--- components/ContentCard/ContentCard.js
+++ components/ContentCard/ContentCard.js
@@ -6,7 +6,7 @@ import { fadeInFromLeft } from '../../animations/default'
 import { makeDatePretty, convertIdToDate } from '../../lib/helpers/dataHelpers'
 import styles from './ContentCard.module.css'
 
-export default function ContentCard ({ activity, source }) {
+export default function ContentCard ({ activities, source, span }) {
   const currentURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.SITE_URI || 'https://whatshouldidotonight.com'
   const getBackground = el => `url(${el.image}) center no-repeat`
   const getContentURL = el => el.pagePath ? `${currentURL}${el.pagePath}` : el.name && `/${source}/activity?name=${el.name.split(' ').join('_')}`
@@ -16,35 +16,40 @@ export default function ContentCard ({ activity, source }) {
     return prettyDate
   }
   return (
-    activity.name
+    activities && activities.length
       ? (
-        <li className={styles.cardListItem}>
-          <Link href={getContentURL(activity)} as={getContentURL(activity)}>
-            <a className={styles.cardLink}>
-              <motion.div
-                className={styles.contentCard}
-                key={activity.name}
-                whileHover={{ scale: 1.02 }}
-                whileTap={{ scale: 0.97 }}
-                variants={fadeInFromLeft}
-              >
-                <div className={styles.topHalf} style={{ background: getBackground(activity), backgroundSize: 'cover' }}>
-                  <div className={styles.ribbon}>
-                    <span className={styles.ribbonText}>{activity.free && 'FREE'}</span>
+        activities.map((activity, idx) => (
+          <li
+            className={styles[span ? 'span' : 'cardListItem']}
+            style={idx + 1 % 3 !== 0 ? { marginRight: '1rem' } : null}
+            key={activity.name}
+          >
+            <Link href={getContentURL(activity)} as={getContentURL(activity)}>
+              <a className={styles.cardLink}>
+                <motion.div
+                  className={styles[span ? 'spanContentCard' : 'contentCard']}
+                  key={activity.name}
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.97 }}
+                  variants={fadeInFromLeft}
+                >
+                  <div className={styles[span ? 'leftHalf' : 'topHalf']} style={{ background: getBackground(activity), backgroundSize: 'cover' }}>
+                    <div className={styles.ribbon}>
+                      <span className={styles.ribbonText}>{activity.free && 'FREE'}</span>
+                    </div>
                   </div>
-                </div>
-                <div className={styles.bottomHalf}>
-                  <div className={styles.infoContainer}>
-                    <div className={styles.cardName}>{activity.name}</div>
-                    <div className={styles.publishDate}>{handleDate(activity._id)}</div>
+                  <div className={styles[span ? 'rightHalf' : 'bottomHalf']}>
+                    <div className={styles.infoContainer}>
+                      <div className={styles.cardName}>{activity.name}</div>
+                      <div className={styles.publishDate}>{handleDate(activity._id)}</div>
+                    </div>
+                    <div className={styles.tagline}>{activity.tagline}</div>
                   </div>
-                  <div className={styles.tagline}>{activity.tagline}</div>
-                </div>
-              </motion.div>
-            </a>
-          </Link>
-        </li>
-
+                </motion.div>
+              </a>
+            </Link>
+          </li>
+        ))
       )
       : null
   )

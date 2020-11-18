f8f20e661c412b8e47f889de1984f40b6353f5df
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-17 21:58:32 -0600

    more site redesign

--- pages/do/all.js
+++ pages/do/all.js
@@ -13,7 +13,7 @@ const categoryDescriptions = {
   learn: { title: 'Fun courses and apps to learn from tonight', header: 'Learn', tag: "Nights are the time to hone your skills and learn new ones. Of course, watching TV is fine, we've got a whole page dedicated to it, in fact! But taking time out to learn sets you apart from most. You've got this." },
   food: { title: 'Tasty food and drink ideas to try tonight', header: 'Food & Drink', tag: 'Batman and Robin, peanut butter and jelly, nights and food. Some pairings just work. Check out all these fun food things you can do. Tonight!' },
   alone: { title: "Fun ideas for things to do when you're alone", header: 'Alone', tag: "Whether you're happy that you're alone tonight or not, we've got something for you." },
-  games: { header: 'Fun game ideas to try tonight', tag: "Nights are made for fun. Games are fun. It's almost like nights were meant for games?!" },
+  games: { title: 'Fun game ideas to try tonight', header: 'Games', tag: "Nights are made for fun. Games are fun. It's almost like nights were meant for games?!" },
 }
 
 function Content () {

70667189eb6b26b106f7a8017a1f279fb7fca96a
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-17 22:05:03 -0600

    fix build break

---
+++ pages/listen2dntGoHere/activity.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import PropTypes from 'prop-types'
+
+// REDUX
+import { connect } from 'react-redux'
+
+// COMPONENTS
+import Layout from '../../components/layout/layout'
+import Loading from '../../components/loading/loading'
+import ArticleHead from '../../components/ArticleHead'
+import HelpfulCounter from '../../components/HelpfulCounter/HelpfulCounter'
+
+// HELPERS
+import utilStyles from '../../styles/utils.module.css'
+import { getActivityFromDB } from '../../lib/helpers/db/requests'
+import ContentDisplay from '../../components/ContentDisplay/ContentDisplay'
+
+function Content () {
+  const [activity, setActivity] = useState({})
+  const router = useRouter()
+  const { name } = router.query
+  const getActivity = async () => {
+    try {
+      const newActivity = await getActivityFromDB('listen', name)
+      setActivity(newActivity)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    if (!activity.name || activity.name !== name) getActivity()
+  }, [name])
+  return (
+    <Layout>
+      {
+        activity
+          ? (
+            <>
+              <ArticleHead activity={activity}/>
+              <div className={utilStyles.watchContentSection}>
+                <ContentDisplay content={activity} />
+                <div className={utilStyles.showInfoSectionContainer}>
+                  <div className={utilStyles.showInfoSectionHeader}>Show Info:</div>
+                  <div className={utilStyles.showInfoSectionBody}>
+                    {
+                      activity && activity.notes && activity.notes.listenSpeed
+                        ? (
+                          <div className={utilStyles.recommendedSpeed}>
+                            <div className={utilStyles.tableHeader}>Listen at:</div>
+                            <div className={utilStyles.showSpeed}>{activity.notes.listenSpeed}x</div>
+                          </div>
+                        )
+                        : null
+                    }
+                    {
+                      activity && activity.notes && activity.notes.notableEpisodes && activity.notes.notableEpisodes.length
+                        ? (
+                          <div className={utilStyles.whereToWatch}>
+                            <div className={utilStyles.tableHeader}>Notable Episodes:</div>
+                            {activity.notes.notableEpisodes.map((obj) => (
+                              <Link key={`${activity.name}-${obj.name}`} href={obj.url} as={obj.url}>
+                                <a target="_blank">{obj.name}</a>
+                              </Link>
+                            ))}
+                          </div>
+                        )
+                        : null
+                    }
+                  </div>
+                </div>
+                <HelpfulCounter activity={activity} source="listen" />
+              </div>
+            </>
+          )
+          : <Loading loading={true}/>
+      }
+    </Layout>
+  )
+}
+
+Content.propTypes = {
+  activity: PropTypes.object,
+}
+
+export default connect((state) => state)(Content)
---
+++ pages/listen2dntGoHere/index.js
@@ -0,0 +1,106 @@
+import React, { useState, useRef } from 'react'
+import Head from 'next/head'
+import PropTypes from 'prop-types'
+
+// REDUX
+import wrapper from '../../src/store/store'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { setListenActivities } from '../../src/store/categories/action'
+
+// COMPONENTS
+import Layout from '../../components/layout/layout'
+import SplashContent from '../../components/SplashContent/SplashContent'
+import HandleContent from '../../components/HandleContent'
+import { siteTitle } from '../../components/defaultHead'
+
+// HELPERS
+import utilStyles from '../../styles/utils.module.css'
+import { getActivitiesFromDB } from '../../lib/helpers/db/requests'
+
+const pageDescription =
+"We've listened to tons of less-known podcasts on Apple, Spotify, Stitcher, and more to give you suggestions about " +
+'the best comedy, news, political, film / movies, educational, tech, self improvement, cultural, and food podcasts to listen to.'
+
+const quotes = [
+  {
+    header: '"You aren’t learning anything when you’re talking."',
+    contents: ['-Lyndon B. Johnson'],
+  },
+  {
+    header: '"The quieter you become, the more you can hear."',
+    contents: ['-Buddha'],
+  },
+]
+
+export const getStaticProps = wrapper.getStaticProps(({ store }) => {
+  const state = store.getState()
+  const all = state.categories.listenActivities.all || {}
+  const spotlight = state.categories.listenActivities.spotlight || []
+  const articles = state.categories.listenActivities.articles || []
+  if (spotlight && !spotlight.length) return getActivitiesFromDB('listen')
+  else {
+    return {
+      props: {
+        spotlight,
+        all,
+        articles,
+      },
+    }
+  }
+})
+
+function ListenSection ({ articles = [], spotlight = [], all = {}, setInRedux, setListenActivitiesFromProps }) {
+  const [updatedRedux, setUpdatedRedux] = useState(false)
+  if (!updatedRedux && setInRedux) {
+    setUpdatedRedux(true)
+    setListenActivitiesFromProps({ all, spotlight })
+  }
+  const source = 'listen'
+  const obj = all || {}
+  const homeRef = useRef('home')
+  const display = (
+    <HandleContent
+      all={obj}
+      articles={articles}
+      source={source}
+      quotes={quotes}
+      homeRef={homeRef}
+    />
+  )
+
+  return (
+    <Layout>
+      <Head>
+        <title>Things to Listen To Tonight - {siteTitle}</title>
+        <meta name="description" content={pageDescription} />
+
+      </Head>
+      {spotlight && Array.isArray(spotlight) && spotlight.length && <SplashContent content={spotlight} banner="Listen to the less known" source={source} />}
+      <div className={utilStyles.infoContainer} ref={homeRef}>
+        <div className={utilStyles.infoHeader}>Do you know how many bad podcasts are out there?</div>
+        <div className={utilStyles.infoBody}>
+          <p>We do. And now you don&apos;t have to.</p>
+          <p>You&apos;re welcome.</p>
+        </div>
+      </div>
+      {display}
+    </Layout>
+  )
+}
+
+ListenSection.propTypes = {
+  spotlight: PropTypes.array,
+  articles: PropTypes.array,
+  all: PropTypes.object,
+  setListenActivitiesFromProps: PropTypes.func,
+  setInRedux: PropTypes.bool,
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setListenActivitiesFromProps: bindActionCreators(setListenActivities, dispatch),
+  }
+}
+
+export default connect((state) => state, mapDispatchToProps)(ListenSection)

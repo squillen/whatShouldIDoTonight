efd319be6fbebca2165b7bdfcacbf3384765a6f1
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-24 09:07:25 -0600

    change from TV to ideas

--- pages/watch/ideas.js
+++ pages/watch/ideas.js
@@ -10,7 +10,7 @@ import GetAllEvents from '../../components/GetAllEvents/GetAllEvents'
 const categoryInfo = {
   title: 'The hidden gems on Netflix, Hulu, Prime, and more to watch tonight',
   header: 'The Stuff To Stream',
-  tag: "Whether you're in the mood for movies, shows, shorts, or vignettes, we've got you covered for all the best hidden gems on Netflix, Hulu, Prime, and more. Let the watching commence.",
+  tag: "What're you in the mood for? Don't know? We got you. Here are a smattering of ideas we have for things to watch right now to fit any mood you have. Happy watching, nighty.",
 }
 
 function Content () {
@@ -18,7 +18,7 @@ function Content () {
     <GetAllEvents
       categoryInfo={categoryInfo}
       source="watch"
-      category="tv"
+      category="ideas"
     />
   )
 }

996eefc127c7cd6e13deb618eee9fb0d8149b3ae
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-27 10:44:07 -0600

    lots of improvements

--- components/photo/photo.js
+++ components/photo/photo.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import PropTypes from 'prop-types'
+import handleMarkdown from '../../lib/helpers/handleMarkdown'
 import styles from './photo.module.css'
 
-export default function Photo ({ src, alt }) {
+export default function Photo ({ src, alt, caption }) {
+  console.log('caption :>> ', caption);
   const [enlargePhoto, setEnlargedPhoto] = useState(false)
   return (
-    <img
-      src={src}
-      alt={alt}
-      className={styles[enlargePhoto ? 'enlargedPhoto' : 'photo']}
-      onClick={() => setEnlargedPhoto(!enlargePhoto)}
-    />
+    <figure className={styles.photoContainer}>
+      <img
+        src={src}
+        alt={alt}
+        className={styles[enlargePhoto ? 'enlargedPhoto' : 'photo']}
+        onClick={() => setEnlargedPhoto(!enlargePhoto)}
+      />
+      {caption && <figcaption className={styles.figcaption}>{handleMarkdown(caption)}</figcaption>}
+    </figure>
   )
 }
 
 Photo.propTypes = {
+  caption: PropTypes.string,
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 }

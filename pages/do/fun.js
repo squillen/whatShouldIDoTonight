26ed40d46f3e76bea8af0c9fc0d2bd046b40b7d1
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-28 17:09:26 -0600

    update seo stuff

--- pages/do/fun.js
+++ pages/do/fun.js
@@ -8,9 +8,10 @@ import { connect } from 'react-redux'
 import GetAllEvents from '../../components/GetAllEvents/GetAllEvents'
 
 const categoryInfo = {
-  title: 'Fun game ideas to try tonight',
-  header: 'Games',
-  tag: "Nights are made for fun. Games are fun. It's almost like nights were meant for games?!",
+  title: 'Fun things to do tonight',
+  description: "Play some games that you know about and some that you've never heard of. Either way, you're having some fun tonight!",
+  header: 'Fun',
+  tag: 'Nights are when the stress of the day is done with and finally get a chance to relax and have fun. These are the things you should do.',
 }
 
 function Content () {
@@ -20,7 +21,7 @@ function Content () {
         <GetAllEvents
           categoryInfo={categoryInfo}
           source="do"
-          category="games"
+          category="fun"
         />
       )
       : null
--- pages/do/self-improvement.js
+++ pages/do/self-improvement.js
@@ -10,6 +10,7 @@ import GetAllEvents from '../../components/GetAllEvents/GetAllEvents'
 const categoryInfo = {
   title: 'Ideas to improve yourself and grow as a person',
   header: 'Self Improvement',
+  description: 'Bored at home and want to improve yourself? Become an even better person tonight by checking out these articles on self-improvement.',
   tag: "Whether you believe it or not, you're awesome. And you can become even more awesome! Awesome! How? By checking out all of our articles!",
 }
 

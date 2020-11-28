b56ad54e4187b085bfee597e500f8b0639a3218d
Sean Quillen <sean.r.quillen@gmail.com>
2020-11-28 17:09:26 -0600

    update seo stuff

--- pages/do/learn.js
+++ pages/do/learn.js
@@ -9,6 +9,7 @@ import GetAllEvents from '../../components/GetAllEvents/GetAllEvents'
 
 const categoryInfo = {
   title: 'Fun courses and apps to learn from tonight',
+  description: 'Find the best courses to learn about history, cultures, wine, beer, and how to code, cook and so much more.',
   header: 'Learn',
   tag: "Nights are the time to hone your skills and learn new ones. Of course, watching TV is fine, we've got a whole page dedicated to it, in fact! But taking time out to learn sets you apart from most. You've got this.",
 }

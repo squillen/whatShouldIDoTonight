9cefb03d5aafcad0dc8d4089c1e3f5f85d78121d
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-10 17:07:44 -0600

    uncomment mailchimp script

--- components/defaultHead.js
+++ components/defaultHead.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types'
 import Head from 'next/head'
 export const siteTitle = 'What Should I Do Tonight - Fun, entertaining things we can do, watch, eat, drink, learn, and listen to right now'
 export const baseDescription = "Need to know what to do when you're bored? Find free things to do tonight at home or outside and things to do near me tonight. "
-// const mailChimpScript = `
-//   !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/42fd643d25e61b586e6992d41/c722f51a5614f406bcfbd1aa9.js")
-// `
+const mailChimpScript = `
+  !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/42fd643d25e61b586e6992d41/c722f51a5614f406bcfbd1aa9.js")
+`
 
 const image = '/logos/logo.png'
 
@@ -34,7 +34,7 @@ export default function DefaultHead ({ title, description }) {
       <meta property="twitter:image" content={image}></meta>
       <title>{usedTitle}</title>
       <meta name="description" content={usedDescription} />
-      {/* <script id="mcjs" dangerouslySetInnerHTML={{ __html: mailChimpScript }} /> */}
+      <script id="mcjs" dangerouslySetInnerHTML={{ __html: mailChimpScript }} />
       <script data-ad-client="ca-pub-9045195637006996" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossOrigin="anonymous" />
     </Head>

526cd8b24ff30cf79c814c52cff11a5a1f1fe0fb
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-09 20:12:39 -0600

    change url

--- components/Disqus/Disqus.js
+++ components/Disqus/Disqus.js
@@ -7,7 +7,7 @@ const Disqus = ({ article, url }) => {
   const disqusShortname = 'whatshouldidotonight'
   const env = url.includes('localhost') ? 'dev' : 'prod'
   const disqusConfig = {
-    url,
+    url: 'https://whatshouldidotonight.com',
     identifier: `${article._id}_${env}`,
     title: article.name,
   }

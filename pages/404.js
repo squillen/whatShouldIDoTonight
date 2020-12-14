1f063bf7fe20a7209780a58d642b9503043c7804
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-14 14:18:47 -0600

    add no index nofollow

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import Button from '../components/button/button'
 import Layout from '../components/layout/layout'
 import utilStyles from '../styles/utils.module.css'
@@ -5,6 +6,10 @@ import utilStyles from '../styles/utils.module.css'
 export default function Custom404 () {
   return (
     <Layout>
+      <NextSeo
+        nofollow={true}
+        noindex={true}
+      />
       <div className={utilStyles.custom404}>
         <p>What you should do tonight is check your url.</p>
         <p>&apos;Cause that shit ain&apos;t right.</p>

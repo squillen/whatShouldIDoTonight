6a8393a20ac8b178caba68f8d2597f764d67ef65
Sean Quillen <sean.r.quillen@gmail.com>
2020-12-14 21:22:53 -0600

    add keywords

--- components/ArticleHead.js
+++ components/ArticleHead.js
@@ -21,6 +21,7 @@ export default function ArticleHead ({ activity = {} }) {
       <ArticleJsonLd
         url={pageURL}
         title={title}
+        keywords={activity.keywords}
         images={[activity.image]}
         authorName={authors}
         datePublished={datePublished}
